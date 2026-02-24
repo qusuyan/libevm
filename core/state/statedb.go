46a37abee81c5409f2b1bb7a7993c69b8b46e077
qusuyan <qusuyan@gmail.com>
2026-02-23 23:09:05 -0600

    bugfix

--- core/state/statedb.go
+++ core/state/statedb.go
@@ -936,7 +936,7 @@ func (s *StateDB) SetTxContext(thash common.Hash, ti int) {
 func (s *StateDB) clearJournalAndRefund() {
 	fmt.Printf("Clear journal: \n")
 	for _, entry := range s.journal.entries {
-		fmt.Printf("\t%T %x: %v\n", entry, *entry.dirtied(), entry)
+		fmt.Printf("\t%T %x: %v\n", entry, entry.dirtied(), entry)
 	}
 	if len(s.journal.entries) > 0 {
 		s.journal = newJournal()
